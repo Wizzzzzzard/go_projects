c2f63bfb08dd7c286aaa91abe2469be28d03aa2f
Elijah Reid <reideli@lab.morganstanley.com>
2022-11-16 11:31:03 +0000

    Added Controllers to implement API request functionality

--- main.go
+++ main.go
@@ -1,31 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
-	"github.com/pluralsight/webservice/models"
+	"github.com/pluralsight/webservice/controllers"
 )
 
 const ()
 
 func main() {
-	u := models.User{
-		ID:        2,
-		FirstName: "Zaphod",
-		LastName:  "Beeblebrox",
-	}
-
-	fmt.Println(u)
-
-	fmt.Println("Hello World")
-	port := 3000
-	_, err := startWebServer(port)
-	fmt.Println(port, err)
-}
-
-func startWebServer(port int) (int, error) {
-	fmt.Println("Starting server...")
-	// do important things
-	fmt.Println("Server started on port", port)
-	return port, nil
+	controllers.RegisterController()
+	http.ListenAndServe(":3000", nil)
 }
